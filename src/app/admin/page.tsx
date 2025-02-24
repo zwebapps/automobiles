ca91e872846ada7c6995407c35e993e8967d9550
Zahoor Ahmed <zdev1989@gmail.com>
2025-02-25 00:49:55 +0100

    added sidebar chagnes

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import {
+  CButton,
   CCol,
   CContainer,
   CNavItem,
@@ -20,9 +21,10 @@ import ServicesSection from "../ServicesSection";
 import Portfolio from "../components/Portfolio";
 import CarCard from "../components/CarCard";
 import { Car, Cars } from "../common/common";
+import { Form } from "react-bootstrap";
 
 export default function Admin() {
-  // const [sidebarShow, setSidebarShow] = useState();
+  const [sidebarShow, setSidebarShow] = useState(true);
   const [togglePage, setTogglePage] = useState({
     header: true,
     about: false,
@@ -49,40 +51,82 @@ export default function Admin() {
      } 
    });  
   }
+
+  const toggleSidebar = () => {
+    setSidebarShow(!sidebarShow);
+  }
   return (
-    <CContainer fluid>
+    <CContainer fluid>     
       <CRow>
-      <CCol className="col-2 p-0 m-0">
-      <CSidebar className="border-end border-start dark" colorScheme="dark">
-      <CSidebarHeader className="border-bottom p-5 text-center">
-        <CSidebarBrand>Majestic Journey</CSidebarBrand>
-      </CSidebarHeader>
-      <CSidebarNav className="text-center border-bottom">
-        <CNavItem href="#" className={`list-unstyled p-3 ${togglePage.header ? 'nav-item-active' : ''}`} onClick={() => handleTogglePage('header')} >
-          <CIcon customClassName="nav-icon pr-1" icon={cilSpeedometer} height={24} width={24} /> Header
-        </CNavItem>
-        <CNavItem href="#" className={`list-unstyled p-3 ${togglePage.about ? 'nav-item-active' : ''}`}  onClick={() => handleTogglePage('about')} >
-          <CIcon customClassName="nav-icon pr-1" icon={cilSpeedometer} height={24} width={24} /> About
-        </CNavItem>
-        <CNavItem  href="#" className={`list-unstyled p-3 ${togglePage.services ? 'nav-item-active' : ''}`}  onClick={() => handleTogglePage('services')}>
-          <CIcon customClassName="nav-icon pr-1" icon={cilCloudDownload} height={24} width={24} />
-           Services
-        </CNavItem>
-        <CNavItem  href="#" className={`list-unstyled p-3 ${togglePage.portfolio ? 'nav-item-active' : ''}`}  onClick={() => handleTogglePage('portfolio')}>
-          <CIcon customClassName="nav-icon pr-1" icon={cilCloudDownload} height={24} width={24} />
-           Portfolio
-        </CNavItem>
-        <CNavItem href="#" className={`list-unstyled p-3 ${togglePage.listing ? 'nav-item-active' : ''}`}  onClick={() => handleTogglePage('listing')}>
-          <CIcon customClassName="nav-icon pr-1" icon={cilCloudDownload} height={24} width={24} />
-           Cars Listing
-        </CNavItem>
-        <CNavItem href="#" className={`list-unstyled p-3 ${togglePage.contact ? 'nav-item-active' : ''}`}  onClick={() => handleTogglePage('contact')}>
-          <CIcon customClassName="nav-icon pr-1 list-unstyled" icon={cilLayers} height={24} width={24}/> Contact Us
-        </CNavItem>
-      </CSidebarNav>
-    </CSidebar>
+        <CCol className="col-12 p-0 m-0 d-lg-none d-sm-block">
+          <CButton 
+              className="navbar-toggler navbar-toggler-right" 
+              type="button" 
+              data-toggle="collapse" 
+              data-target="#navbarResponsive" 
+              aria-controls="navbarResponsive" 
+              aria-expanded="false" 
+              aria-label="Toggle navigation"
+              style={{ background: '#fefefe', zIndex: 1}}
+              onClick={toggleSidebar}
+            >
+              <span className="navbar-toggler-icon"></span> 
+            </CButton>
       </CCol>
-      <CCol className="col-10 p-0 m-0">
+      <CCol className="col-2 p-0 m-0" id="mainNav">
+        <CContainer style={{ background: '#ffffff'}}>     
+        <CSidebar className="border-end border-start dark collapse navbar-collapse"  style={{ background: '#ffffff', zIndex: 1, display: `${sidebarShow ? 'block' : 'none'}` }} colorScheme="dark" position="fixed">
+        <CSidebarHeader className="border-bottom p-5 text-center">
+          <CSidebarBrand>Majestic Journey</CSidebarBrand>
+        </CSidebarHeader>
+        <CSidebarNav className="text-center border-bottom">
+          <CNavItem href="#" className={`list-unstyled p-3 ${togglePage.header ? 'nav-item-active' : ''}`} onClick={() => handleTogglePage('header')} >
+            <CIcon customClassName="nav-icon pr-1" icon={cilSpeedometer} height={24} width={24} /> Header
+          </CNavItem>
+          <CNavItem href="#" className={`list-unstyled p-3 ${togglePage.about ? 'nav-item-active' : ''}`}  onClick={() => handleTogglePage('about')} >
+            <CIcon customClassName="nav-icon pr-1" icon={cilSpeedometer} height={24} width={24} /> About
+          </CNavItem>
+          <CNavItem  href="#" className={`list-unstyled p-3 ${togglePage.services ? 'nav-item-active' : ''}`}  onClick={() => handleTogglePage('services')}>
+            <CIcon customClassName="nav-icon pr-1" icon={cilCloudDownload} height={24} width={24} />
+            Services
+          </CNavItem>
+          <CNavItem  href="#" className={`list-unstyled p-3 ${togglePage.portfolio ? 'nav-item-active' : ''}`}  onClick={() => handleTogglePage('portfolio')}>
+            <CIcon customClassName="nav-icon pr-1" icon={cilCloudDownload} height={24} width={24} />
+            Portfolio
+          </CNavItem>
+          <CNavItem href="#" className={`list-unstyled p-3 ${togglePage.listing ? 'nav-item-active' : ''}`}  onClick={() => handleTogglePage('listing')}>
+            <CIcon customClassName="nav-icon pr-1" icon={cilCloudDownload} height={24} width={24} />
+            Cars Listing
+          </CNavItem>
+          <CNavItem href="#" className={`list-unstyled p-3 ${togglePage.contact ? 'nav-item-active' : ''}`}  onClick={() => handleTogglePage('contact')}>
+            <CIcon customClassName="nav-icon pr-1 list-unstyled" icon={cilLayers} height={24} width={24}/> Contact Us
+          </CNavItem>
+        </CSidebarNav>
+        </CSidebar>
+        </CContainer>
+      </CCol>
+      
+      <CCol className="col-10 p-0 m-0" >
+        <CCol>
+          <CRow>
+            <CCol className="col-12 pt-5 m-0 text-center">
+              <h1>Header</h1>
+            </CCol>
+          </CRow>
+          <Form>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicCheckbox" />
+          </Form>
+        </CCol>
+      <hr/>
+
         {          
           togglePage.header && (
             <MainHeader></MainHeader>
