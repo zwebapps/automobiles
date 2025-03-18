9182b4bad83208b8194ed0d095a45ab8b2a146d3
Zahoor Ahmed <zdev1989@gmail.com>
2025-03-18 22:02:34 +0100

    Addede changes for env

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
+import dotenv from 'dotenv';
+dotenv.config();
 
 const nextConfig: NextConfig = {
-  
+    env: {
+        MONGODB_URI: process.env.MONGODB_URI,
+        NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+        MONGO_ROOT_USERNAME: process.env.MONGO_ROOT_USERNAME,
+        MONGO_ROOT_PASSWORD: process.env.MONGO_ROOT_PASSWORD,
+        JWT_SECRETE_STRING: process.env.JWT_SECRETE_STRING
+      }
 };
 
 export default nextConfig;
--- src/app/api/post/[identifier]/route.ts
+++ src/app/api/post/[identifier]/route.ts
@@ -1,4 +1,4 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { PostController } from "../../controllers/PostController";
 
 const postController = new PostController();
@@ -6,11 +6,13 @@ const postController = new PostController();
 export async function DELETE(req: NextRequest) {
     const { pathname } = req.nextUrl;
     const postId = pathname.split('/').pop() as string;
-    return await postController.deletePost(req, postId);
+    const post =  await postController.deletePost(req, postId);
+    return NextResponse.json(post, { status: 200 });
   }
 
   export async function GET(req: NextRequest) {
     const { pathname } = req.nextUrl;    
     const postIdOrName = pathname.split('/').pop(); 
-    return await postController.getPostByNameOrId(req, postIdOrName!);
+    const post = await postController.getPostByNameOrId(req, postIdOrName!);
+    return NextResponse.json(post, { status: 200 });
   }
